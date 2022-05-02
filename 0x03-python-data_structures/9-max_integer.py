7ae87fddbb9aa4378cc075e09948216ad2094229
yuan-fang-228 <fy0228@gmail.com>
2022-05-01 21:11:44 -0700

    added return 0

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list == '':
-        return None
+        return 0, None
     maxnumber = my_list[0]
     for number in my_list:
         if number > maxnumber:
