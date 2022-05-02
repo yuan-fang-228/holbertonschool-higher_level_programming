51e7869b61e22dc635c9654cf94e75006e5be32f
yuan-fang-228 <fy0228@gmail.com>
2022-05-01 21:14:07 -0700

    changed the null condition

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == '':
-        return 0, None
+    if len(my_list) == 0:
+        return None
     maxnumber = my_list[0]
     for number in my_list:
         if number > maxnumber:
