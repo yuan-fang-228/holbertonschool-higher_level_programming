224c3ac42c716558fa06f9401f075494e24144b3
yuan-fang-228 <fy0228@gmail.com>
2022-05-04 05:56:15 -0700

    task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_dic = {
+                    "I": 1, "V": 5, "X": 10, "L": 50,
+                    "C": 100, "D": 500, "M": 1000
+                }
+    roman_string = roman_string.replace("IV", "IIII")
+    roman_string = roman_string.replace("IX", "VIIII")
+    roman_string = roman_string.replace("XL", "XXXX")
+    roman_string = roman_string.replace("XC", "LXXXX")
+    roman_string = roman_string.replace("CD", "CCCC")
+    roman_string = roman_string.replace("CM", "DCCCC")
+    number = 0
+    for key in roman_string:
+        number = number + roman_dic[key]
+    return number
