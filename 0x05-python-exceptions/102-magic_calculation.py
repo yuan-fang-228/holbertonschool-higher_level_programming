cd7550f5ffc122cc6622061caa420a00af08f627
yuan-fang-228 <fy0228@gmail.com>
2022-05-16 06:55:35 -0700

    task 9

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -6,7 +6,7 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception('Too far')
             result = result + a ** b / i
-        except:
+        except Exception:
             result = b + a
             break
     return result
