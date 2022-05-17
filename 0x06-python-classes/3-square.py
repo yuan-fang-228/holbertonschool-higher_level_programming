b1196038be9c88a632bbe9f01943f0e32675537d
yuan-fang-228 <fy0228@gmail.com>
2022-05-17 05:46:43 -0700

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Represent a Square class
+    Private instance attribute: size
+    Instantiation with optional size
+"""
+
+
+class Square:
+    """Represent a Square class"""
+    def __init__(self, size=0):
+        """Initialize the square with private instance attribute"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculate the current square area"""
+        return self.__size * self.__size
