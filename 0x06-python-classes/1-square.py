deb4348d72ac180a1a0d51e965bd6acf550025fc
yuan-fang-228 <fy0228@gmail.com>
2022-05-17 05:20:54 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Represent a Square class
+    Private instance attribute: size
+    Instantiation with size (no type/value verification)
+"""
+
+
+class Square:
+    """Represent a Square class"""
+    def __init__(self, size):
+        """Initialize the square"""
+        self.__size = size
