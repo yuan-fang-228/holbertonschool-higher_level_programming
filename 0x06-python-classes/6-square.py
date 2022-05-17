e1919d07e57946c22c4639fd43b820d39035ba1c
yuan-fang-228 <fy0228@gmail.com>
2022-05-17 07:14:11 -0700

    fixed logic

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -54,11 +54,13 @@ class Square:
         """ Prints in stdout the square with the character #"""
         if self.__size == 0:
             print("")
-        for i in range(self.__position[1]):
-            print()
-        for s in range(self.__size):
-            for r in range(self.__position[0]):
-                print(" ", end="")
-            for c in range(self.__size):
-                print("#", end="")
-            print()
+        else:
+            if self.position[1] > 0:
+                for i in range(self.__position[1]):
+                    print()
+            for s in range(self.__size):
+                for r in range(self.__position[0]):
+                    print(" ", end="")
+                for c in range(self.__size):
+                    print("#", end="")
+                print()
