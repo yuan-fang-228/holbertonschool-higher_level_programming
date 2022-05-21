f8062bc6e08c32ce6b3a04b9ac2c1d48c258efae
yuan-fang-228 <fy0228@gmail.com>
2022-05-20 23:21:32 -0700

    added description to fucntion

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -7,6 +7,10 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """a function that prints fist name and last name
+        fist name and last name must be string
+        otherwise TypeError will be raised"""
+
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
