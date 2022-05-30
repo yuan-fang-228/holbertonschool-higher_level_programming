97426fac8449f60ab3e93927e66c10c000ca443f
yuan-fang-228 <fy0228@gmail.com>
2022-05-30 02:49:01 -0700

    added comments for task 5 and 6

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""5-base_geometry"""
+
+
 class BaseGeometry:
     """an empty class"""
     pass
--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""6-base_geometry module"""
+
+
 class BaseGeometry:
+    """represent a class"""
+
     def area(self):
         """raise an exception with message
             "area() is not implemented"
