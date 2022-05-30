74550cea45cce69e8a2a7b69246aa0acc44de3e5
yuan-fang-228 <fy0228@gmail.com>
2022-05-30 03:01:26 -0700

    add validator

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -11,4 +11,5 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """initialise the instance of square"""
+        self.integer_validator("size", size)
         super().__init__(size, size)
