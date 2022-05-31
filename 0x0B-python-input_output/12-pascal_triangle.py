7982533bd9cd63fd8679f4829da8854948ef40be
yuan-fang-228 <fy0228@gmail.com>
2022-05-31 07:27:46 -0700

    changed logic

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -4,14 +4,15 @@
 
 def pascal_triangle(n):
     """return a list of lists of integers representing Pascal's triangle"""
-    array = [[1]]
-    if n == 1:
-        return array
-    for row in range(0, n-1):
-        li = [1]
-        for col in range(0, row):
-            li.append(array[-1][col] + array[-1][col + 1])
-        li.append(1)
-        array.append(li)
+    if n < 0:
+        return []
+    array = []
+    for row in range(0, n):
+        array.append([])
+        array[row].append(1)
+        for col in range(1, row):
+            array[row].append(array[row-1][col-1] + array[row-1][col])
+        if (row != 0):
+            array[row].append(1)
 
     return array
