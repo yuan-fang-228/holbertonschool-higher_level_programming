0ac6814195a6a2cdf997d36b111d40f13fe37bf1
yuan-fang-228 <fy0228@gmail.com>
2022-05-31 05:28:01 -0700

    task

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""10-student module"""
+
+
+class Student:
+    """define a student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """instantialize the class with first name, last name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieve a dictionary represnetation of a student instance
+            if attrs is list, only retrieve the attribute names in the list
+        """
+        my_dict = {}
+        if type(attrs) is list:
+            for item in attrs:
+                if type(item) is not str:
+                    return vars(self)
+            for el in attrs:
+                if hasattr(self, el):
+                    my_dict[el] = getattr(self, el)
+            return my_dict
+
+        return vars(self)
