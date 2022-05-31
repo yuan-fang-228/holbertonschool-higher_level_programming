c3d0a05ff952db5cfdc0fad890ffcb9b2bc9ca26
yuan-fang-228 <fy0228@gmail.com>
2022-05-31 06:56:46 -0700

    task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""12-pascal_triangle module"""
+
+
+def pascal_triangle(n):
+    """return a list of lists of integers representing Pascal's triangle"""
+    if n < 0:
+        return []
+    array = [[1]]
+    if n == 1:
+        return array
+    for row in range(0, n-1):
+        li = [1]
+        for col in range(0, row):
+            li.append(array[-1][col] + array[-1][col + 1])
+        li.append(1)
+        array.append(li)
+
+    return array
