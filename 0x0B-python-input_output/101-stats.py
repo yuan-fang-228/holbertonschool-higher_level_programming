5a6803da1bc660d5ac53b0dc53dea995aacda071
yuan-fang-228 <fy0228@gmail.com>
2022-06-01 07:47:59 -0700

    task 14

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""101-stats module"""
+
+
+import sys
+
+
+def print_stats(stats_size, size):
+    print(f"File size: {size}")
+    for key in sorted(stats_size):
+        print(f"{key}: {stats_size[key]}")
+
+
+if __name__ == "__main__":
+
+    stats_size = {
+        200: 0,
+        301: 0,
+        400: 0,
+        401: 0,
+        403: 0,
+        404: 0,
+        405: 0,
+        500: 0
+    }
+    file_size = 0
+    line_count = 0
+
+    try:
+        for lines in sys.stdin:
+            token = lines.split()
+            file_size = file_size + int(token[-1])
+            key = int(token[-2])
+            if key in stats_size:
+                stats_size[key] += 1
+            line_count = line_count + 1
+            if line_count % 10 == 0:
+                print_stats(stats_size, file_size)
+    except KeyboardInterrupt:
+        print_stats(stats_size, file_size)
