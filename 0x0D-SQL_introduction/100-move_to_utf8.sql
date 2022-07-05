a49ad8ffe5ec928c502a72c8f8d5ce7be4ca79e4
yuan-fang-228 <fy0228@gmail.com>
2022-07-05 06:30:34 -0700

    task 17

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- convert hbtn_0c-0 database to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
