1d018a6d7fa3d55d595023689dde9b7eef480133
yuan-fang-228 <fy0228@gmail.com>
2022-07-06 01:52:10 -0700

    task 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- create a database and table cities with id, state_id and name
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
