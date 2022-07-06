b76d2dbe9761d42786488e69a7f87aec5391c17e
yuan-fang-228 <fy0228@gmail.com>
2022-07-05 22:17:37 -0700

    task 3 + 4

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,2 @@
+-- create a table named force_name with id and name
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) not NULL);
---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,2 @@
+-- create a table id_not_null with defaul id value and name
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name  VARCHAR(256));
