5a2b58d87c8ac64fb61a056520ac48893d88ff27
yuan-fang-228 <fy0228@gmail.com>
2022-08-02 13:26:58 +1000

    clean up

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  let index = 0;
-  for (index = 0; index < list.length; index++) {
+  for (let index = 0; index < list.length; index++) {
     if (list[index] === searchElement) {
       count++;
     }
