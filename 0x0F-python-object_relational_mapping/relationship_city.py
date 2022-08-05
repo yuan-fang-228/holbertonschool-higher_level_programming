b0c862fb1ffe3d3805d23832f2c45e4d489d40e9
yuan-fang-228 <fy0228@gmail.com>
2022-08-04 23:35:15 -0700

    update

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -3,6 +3,7 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import ForeignKey
+from relationship_state import Base, State
 
 Base = declarative_base()
 
@@ -12,4 +13,4 @@ class City(Base):
     __tablename__ = "cities"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state_id = Column(Integer, ForeignKey("State.id"), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-"""define a State and link a class to database"""
+"""
+Defines class State
+"""
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from relationship_city import City, Base
 
 Base = declarative_base()
 
 
 class State(Base):
-    """define a class of State inherits from Base"""
+    """
+    Class State; instance of Base
+    Linked to MySQL table "states"
+    """
     __tablename__ = "states"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
+
+    cities = relationship("City", backref="state")
