fa3be2ef8952229256329680f0ff9453a1dd5bf1
yuan-fang-228 <fy0228@gmail.com>
2022-08-04 20:02:05 -0700

    add validation for result

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
     rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    if rows:
+        for row in rows:
+            print(row)
     cur.close()
     db.close()
