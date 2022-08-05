489ec575baa10c969722e1b0adcde52331922d7a
yuan-fang-228 <fy0228@gmail.com>
2022-08-04 19:56:26 -0700

    fix pycodesttyle

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    newState = State(name = "Louisiana")
+    newState = State(name="Louisiana")
     session.add(newState)
     session.commit()
     print(newState.id)
