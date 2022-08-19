a2dc416585daf71c615d9de303bdcaf85c331c7e
yuan-fang-228 <fy0228@gmail.com>
2022-08-19 06:11:00 -0700

    consider letter is empty

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,8 +5,9 @@ import sys
 
 
 if __name__ == "__main__":
-    letter = ""
     if len(sys.argv) == 1:
+        letter = ""
+    else:
         letter = sys.argv[1]
     value = {'q': letter}
     p = requests.post('http://0.0.0.0:5000/search_user', data=value)
