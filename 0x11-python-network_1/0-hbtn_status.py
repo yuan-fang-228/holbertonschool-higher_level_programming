02aba4f00dbc5310fc7c1837dae2a0c1e9022ecb
yuan-fang-228 <fy0228@gmail.com>
2022-08-18 18:45:22 -0700

    pycodestyle

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,10 +4,10 @@ import urllib.request
 
 
 if __name__ == "__main__":
-	req = urllib.request.Request('https://intranet.hbtn.io/status')
-	with urllib.request.urlopen(req) as response:
-		html = response.read()
-		print('Body response:')
-		print('\t - type: {}'.format(type(html)))
-		print('\t - content: {}'.format(html))
-		print('\t - utf8 content: {}'.format(html.decode('utf-8')))
+    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print('Body response:')
+        print('\t - type: {}'.format(type(html)))
+        print('\t - content: {}'.format(html))
+        print('\t - utf8 content: {}'.format(html.decode('utf-8')))
