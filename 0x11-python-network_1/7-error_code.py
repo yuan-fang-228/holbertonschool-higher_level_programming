58be606f1ea710043a9b9b1e92b3d8d1c9cc3d0d
yuan-fang-228 <fy0228@gmail.com>
2022-08-18 20:29:51 -0700

    task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""use requests to send request and handle error"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    req = requests.get(sys.argv[1])
+    if req.status_code < 400:
+        print(req.text)
+    print("Error code: {}".format(req.status_code))
