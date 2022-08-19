b9bebae8d5526146f75aa16b0971cd3bb6bd017e
yuan-fang-228 <fy0228@gmail.com>
2022-08-18 20:31:58 -0700

    add else

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,4 +8,5 @@ if __name__ == "__main__":
     req = requests.get(sys.argv[1])
     if req.status_code < 400:
         print(req.text)
-    print("Error code: {}".format(req.status_code))
+    else:
+        print("Error code: {}".format(req.status_code))
