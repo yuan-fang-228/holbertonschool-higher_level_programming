a10129b5f51750895c88b728361094ee1997e9ee
yuan-fang-228 <fy0228@gmail.com>
2022-08-19 06:38:31 -0700

    typo

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,4 +7,4 @@ from requests.auth import HTTPBasicAuth
 if __name__ == "__main__":
     username_psw = HTTPBasicAuth(sys.argv[1], sys.argv[2])
     response = requests.get("https://api.github.com/user", auth=username_psw)
-    print(response.json.get('id'))
+    print(response.json().get('id'))
