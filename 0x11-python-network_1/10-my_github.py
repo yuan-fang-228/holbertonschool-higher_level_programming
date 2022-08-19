15096f4b5d514ccf5af8b915ec4ada12c9aa9bc3
yuan-fang-228 <fy0228@gmail.com>
2022-08-19 06:33:00 -0700

    task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""use GITHub API to display id based on given the username and password"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+if __name__ == "__main__":
+    username_psw = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    response = requests.get("https://api.github.com/user", auth=username_psw)
+    print(response.json.get('id'))
+
