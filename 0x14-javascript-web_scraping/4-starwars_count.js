e5f453e704db0e27ad7a6ebbd8b6eaca72825b57
yuan-fang-228 <fy0228@gmail.com>
2022-09-13 05:42:16 -0700

    task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const axios = require('axios');
+axios.get(process.argv[2])
+  .then(response => {
+    const movies = response.data.results;
+    const count = movies.filter(movie => movie.characters.includes('https://swapi-api.hbtn.io/api/people/18/')).length;
+    console.log(count);
+  });
